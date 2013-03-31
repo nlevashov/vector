4e788028f87be40012a10a0ddd5205eaa6160845
nlevashov <niklevashov@yandex.ru>
2013-03-31 21:46:09 +0400

    comfortable vector

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include "vector.h"
+#include <iostream>
+using namespace std;
+using namespace myvector;
+
+int main ()
+{
+//	while (1) {
+		vector<int> a;
+//		vector<int> a(80);
+
+		vector<int> b = a;
+//		vector<double> c = a;
+
+		vector<int> d(8); d = a;
+//		vector<float> e(25); e = a;
+
+		cout << a.size() << ' ' << a.memory() << endl;
+
+		for (int i = 0; i < 257; i++) { a[i] = i * 10; cout << a.size() << ' ' << a.memory() << " | "; }
+		cout << endl;
+		for (int i = 0; i < a.size(); i++) cout << a[i] << ' ';
+		cout << endl;
+
+		vector<int> f(7); f = a;
+//		vector<float> g(9); g = a;
+
+		a.resize(80);
+		cout << a.size() << ' ' << a.memory() << endl;
+
+		a.resize(129);
+		cout << a.size() << ' ' << a.memory() << endl;
+//	}
+
+	return 0;
+}
---
+++ vector.h
@@ -0,0 +1,84 @@
+#include <cstdlib>
+#include <cstring>
+
+namespace myvector {
+	template <typename T = double>
+	class vector {
+		private:
+			T * _begin;
+			size_t _size, _memory;
+
+		public:
+			vector();
+			vector(size_t);
+			vector(vector const &);
+			~vector();
+
+			size_t size() { return _size; }
+			size_t memory() { return _memory; }
+
+			vector operator = (vector const &);
+			T & operator [] (size_t i);
+			void resize(size_t n);
+	};
+
+	template <typename T>
+	vector<T> :: vector() : _memory(64), _size(0)
+	{
+		_begin = new T[64];
+	}
+
+	template <typename T>
+	vector<T>::vector(size_t n) : _memory(n), _size(n)
+	{
+		_begin = new T[n];
+		_memory = n;
+		_size = n;
+	}
+
+	template <typename T>
+	vector<T>::vector(vector const & v)
+	{
+		_begin = new T[v._memory];
+		memcpy(_begin, v._begin, sizeof(T) * v._memory);
+		_memory = v._memory;
+		_size = v._size;
+	}
+
+	template <typename T>
+	vector<T>::~vector()
+	{
+		delete _begin;
+	}
+
+	template <typename T>
+	vector<T> vector<T>::operator = (vector<T> const & v)
+	{
+		_begin = (T *) realloc(_begin, sizeof(T) * v._memory);
+		memcpy(_begin, v._begin, sizeof(T) * v._memory);
+		_memory = v._memory;
+		_size = v._size;
+		return *this;
+	}
+
+	template <typename T>
+	T & vector<T>::operator [] (size_t i)
+	{
+		if (i >= _size) {
+			_size = i + 1;
+			if (i >= _memory) {
+				while (_memory <= i) _memory <<= 2;
+				_begin = (T *) realloc(_begin, sizeof(T) * _memory);
+			}
+		}
+		return _begin[i];
+	}
+
+	template <typename T>
+	void vector<T>::resize(size_t n)
+	{
+		_begin = (T *) realloc(_begin, sizeof(T) * n);
+		_memory = n;
+		if (_size > n) _size = n;
+	}
+}
